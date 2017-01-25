af3c461ab45270b634930690a39b08799d5726d0
nikhilshirude <nikhil.shirude@gmail.com>
2017-01-24 22:41:59 -0500

    initial commit for python program

---
+++ PycharmProjects/YouTube/first.py
@@ -0,0 +1,14 @@
+age = 28
+name = "Nikhil"
+
+if age > 31:
+    print("Buy beer")
+elif age > 27 and age < 31:
+    print("Maybe")
+else:
+    print("No")
+
+if name is "Nikhil":
+    print("Cool")
+elif name is "Bhavna":
+    print("Cooool")
